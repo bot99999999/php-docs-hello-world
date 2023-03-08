a06f9596fedf7a5e5ac1921cf0c51fbe20495c85
bot99999999 <60618876+bot99999999@users.noreply.github.com>
2023-03-08 14:20:03 +0100

    NeuHTML/PHP

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-
-echo "Hello Tim!";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Handy-Modelle</title>
+</head>
+<body>
+	<h1>Handy-Modelle</h1>
+	<ul>
+		<li>Apple iPhone 13</li>
+		<li>Samsung Galaxy S21</li>
+		<li>OnePlus 9 Pro</li>
+		<li>Google Pixel 6</li>
+		<li>Xiaomi Mi 12</li>
+	</ul>
+</body>
+</html>
